d3454be58b02cee658c38011ced9f6086fb7b766
LHyeon <dkh1001@outlook.kr>
2025-01-09 23:40:31 +0900

    Feat] Add point color in today

--- src/libs/store/DataStore.ts
+++ src/libs/store/DataStore.ts
@@ -1,12 +1,14 @@
 import { create } from 'zustand';
 
 interface DataInfo {
+  today: Date;
   selDate: Date;
 
   setSelDate: (date: Date) => void;
 }
 
 export const dataStore = create<DataInfo>(set => ({
+  today: new Date(),
   selDate: new Date(),
 
   setSelDate: (date: Date) => {
