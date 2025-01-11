78d5283ad10e5cde38e5376ce037010895f71d49
LHyeon <dkh1001@outlook.kr>
2025-01-11 20:08:01 +0900

    Feat] Make back to today button

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -4,8 +4,10 @@ import Text from '../components/atoms/Text';
 import Calendar from '../components/organisms/Calendar';
 import { dataStore } from '../libs/store/DataStore';
 
+import Undo from '../assets/icons/undo.svg?react';
+
 const CalendarPage = () => {
-  const { selDate, setSelDate } = dataStore();
+  const { today, selDate, setSelDate } = dataStore();
 
   const changeMonth = (dir: boolean) => {
     let y = selDate.getFullYear(),
@@ -28,32 +30,45 @@ const CalendarPage = () => {
     setSelDate(new Date(y, m, 1));
   };
 
+  const goToToday = () => {
+    setSelDate(today);
+  };
+
   return (
     <main>
-      <Flex align="center" gap="2dvw">
-        <Button
-          width="50px"
-          height="50px"
-          type="transparent"
-          onClick={() => {
-            changeMonth(false);
-          }}>
-          〈
-        </Button>
-        <Text>
-          {selDate.getFullYear()}년 {selDate.getMonth() + 1}월
-        </Text>
-        <Button
-          width="50px"
-          height="50px"
-          type="transparent"
-          onClick={() => {
-            changeMonth(true);
-          }}>
-          〉
-        </Button>
+      <Flex width="85%" align="center">
+        <Flex gap="2dvw">
+          <Button
+            width="50px"
+            height="50px"
+            type="transparent"
+            onClick={() => {
+              changeMonth(false);
+            }}>
+            〈
+          </Button>
+          <Text>
+            {selDate.getFullYear()}년 {selDate.getMonth() + 1}월
+          </Text>
+          <Button
+            width="50px"
+            height="50px"
+            type="transparent"
+            onClick={() => {
+              changeMonth(true);
+            }}>
+            〉
+          </Button>
+        </Flex>
+        <Flex style={{ position: 'absolute', right: '7.5%' }} align="flex-end">
+          <Button onClick={goToToday} width="40px" height="40px" fontSize={1.5}>
+            ↺
+          </Button>
+        </Flex>
       </Flex>
-      <Calendar />
+      <Flex direction="column" gap="10px">
+        <Calendar />
+      </Flex>
     </main>
   );
 };
